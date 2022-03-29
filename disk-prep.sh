cbc2b6659d05ae42e7746f35a47dc7256aef4113
Denis Egorov <dnegorov@gmail.com>
2022-03-29 22:54:05 +0300

    function

---
+++ disk-prep.sh
@@ -0,0 +1,42 @@
+#./bin/bash
+
+source $(dirname "$0")/config
+source $(dirname "$0")/functions.sh
+
+function get_disk_role() {
+    if [[ -n $(echo ${CS_DISK_LIST[@]} | grep $1) ]]
+        then
+            echo "CS"
+        elif [ $1 == $MDS_DISK_NAME ]
+            then
+                echo "MDS"
+        else
+            echo "error"
+        fi
+}
+
+if [[ "${MDS_ENABLE^^}" == "NO" ]] && [[ -n "${MDS_DISK_NAME}" ]]
+    then
+        MDS_RESERVED_DISK="RESERVED"
+    fi
+
+echo -e "\n\n"
+echo -e "PARAMETERS:"
+echo -e "   Storage name:    " $VSTOR_NMAE
+echo -e "   CS names policy: " "by" $CS_NAMES_POLICY
+echo -e "   MDS enabled:     " ${MDS_ENABLE^^}
+echo -e "   MDS disk name:   " $MDS_DISK_NAME "$MDS_RESERVED_DISK"
+echo -e "   CS disk list:    " ${CS_DISK_LIST[@]}
+echo -e "\n\n"
+
+# sleep 3s
+
+for disk in $MDS_DISK_NAME ${CS_DISK_LIST[@]}
+    do
+        case $(get_disk_type $disk) in
+            1) d_type="hdd";;
+            0) d_type="ssd";;
+            *) d_type="error"
+        esac
+        echo /dev/$disk $d_type $(get_disk_role $disk)
+    done
---
+++ functions.sh
@@ -0,0 +1,13 @@
+#./bin/bash
+
+# Get disk type
+# return 0 for ssd
+# return 1 for hdd
+# emty if erorr
+function get_disk_type() {
+    if [ -n $1 ] && [ -e /sys/block/$1/queue/rotational ]
+        then
+            cat /sys/block/$1/queue/rotational
+        fi
+}
+
